7fb012ed10a8f745be587d51413df0df0f9e5796
Admin <Admin@DESKTOP-TP18Q4P>
2020-08-09 00:55:42 +0530

    Commit test

---
+++ Testing2020/src/test/java/JAVAPROGRAMME/constructor/exmple.java
@@ -0,0 +1,15 @@
+package JAVAPROGRAMME.constructor;
+
+public class exmple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=5;
+		
+		System.out.println("add "+a);
+		
+	}
+
+}
